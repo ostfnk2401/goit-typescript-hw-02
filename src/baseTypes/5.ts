8a887e1f835ae42f24a8928ebfacc8a0d8592ca8
ostfnk2401 <117888795+ostfnk2401@users.noreply.github.com>
2023-09-24 22:07:07 +0200

    123

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,4 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let union: string | number;
